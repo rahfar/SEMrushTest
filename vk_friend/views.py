ce773f3690cc94bc4d5aa2bcc281e120f12fbc24
rahfar <rahfar777@gmail.com>
2020-03-03 02:10:58 +0300

    fix

--- vk_friend/views.py
+++ vk_friend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_GET
 
-# Create your views here.
+
+@require_GET
+def home(request):
+    return render(request, 'index.html')
